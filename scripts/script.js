1f2c63d0a3da239f3b4d25612863a5e87ae5b4cf
James Gower <james_gower@outlook.com>
2017-11-01 11:22:56 +0000

    Portfolio
    Basic Personal Portfolio

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $("#nav-bar-popup").hide();
+    
+    $(document).scroll(function () {
+        var y = $(this).scrollTop();
+        var topOfDiv = $(".seperator").offset().top-40;
+        if (y > topOfDiv) {
+            $('#nav-bar-popup').fadeIn(1000);
+        }
+        else {
+            $("#nav-bar-popup").fadeOut(1000);
+        }
+    });
+    
+    $("#arrow").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#skillsTop").offset().top
+        }, 2000);
+    });
+    
+    $(".home").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#homepage").offset().top
+        }, 2000);
+    });
+
+    $(".skills").click(function(e){
+        $('html, body').animate({
+            scrollTop: $("#skillsTop").offset().top
+        }, 2000);
+    });
+    
+    $(".whatIDo").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#whatIDoTop").offset().top
+        }, 2000);
+    });
+    
+    $(".portfolio").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#portfolioTop").offset().top
+        }, 2000);
+    });
+
+    $(".contactMe").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contactTop").offset().top
+        }, 2000);
+    });
+});
