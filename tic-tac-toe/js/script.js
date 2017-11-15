34307c4309a3714dac320c287535440feb9350f9
James Gower <james_gower@outlook.com>
2017-11-15 17:09:23 +0000

    fully working tic-tac-toe page with minimax algorithm added

--- tic-tac-toe/js/script.js
+++ tic-tac-toe/js/script.js
@@ -1,17 +1,111 @@
 /* jshint esnext: true */
 var noPlayers;
-var difficulty = 2;
-$(document).ready(function(){
-    $("#singlePlayer").click(function(){
+var difficulty = 1;
+var huPlayer = "X";
+var huPlayer2 = "O";
+var aiPlayer = "O";
+var p1name;
+var p2name;
+var currentPlayer = 1;
+var p1score = 0;
+var p2score = 0;
+var aiscore = 0;
+
+$(document).ready(function () {
+    $("#singlePlayer").click(function () {
         $(".startgame").fadeOut(1000);
+        $("#player2").remove();
+        $(".player").css("display", "block");
+        $(".aiChoice").css("display", "block");
+        $(".title").css("margin-top", "70px");
+        $(".title").text("Enter your name and choose the AI difficulty!");
+        $(".choice").fadeIn(1000);
         noPlayers = 1;
+    });
+    $("#twoPlayer").click(function () {
+        $(".aiChoice").remove();
+        $(".startgame").fadeOut(1000);
+        noPlayers = 2;
+    });
+
+    $("#switchChoice").click(function () {
+        if ($("#p1choice").text() === "O") {
+            $("#p1choice").text("X");
+            $("#p2choice").text("O");
+            aiPlayer = "X";
+        } else {
+            $("#p1choice").text("O");
+            $("#p2choice").text("X");
+            aiPlayer = "O";
+        }
+    });
+
+    $("#confirm").click(function () {
+        if (noPlayers === 1) {
+            if ($("#p1name").val().length === 0) {
+                $("#player1name").text("Player 1: ");
+            } else {
+                p1name = $("#p1name").val();
+                $("#player1name").text(p1name + ": ");
+            }
+            if ($("#p1choice").text() === "X") {
+                huPlayer = "X";
+                aiPlayer = "O";
+            } else {
+                huPlayer = "O";
+                aiPlayer = "X";
+            }
+            $("#player2name").text("Computer: ");
+        } else {
+            if ($("#p1name").val().length === 0) {
+                p1name = "Player 1";
+                $("#player1name").text(p1name);
+            } else {
+                p1name = $("#p1name").val();
+                $("#player1name").text(p1name);
+                if ($("#p1choice").text() === "X") {
+                    huPlayer = "X";
+                    huPlayer2 = "O";
+                } else {
+                    huPlayer = "X";
+                    huPlayer2 = "O";
+                }
+            }
+            if ($("#p2name").val().length === 0) {
+                p2name = "Player 2:";
+                $("#player2name").text(p2name);
+            } else {
+                p2name = $("#p2name").val();
+                $("#player2name").text(p2name);
+            }
+        }
+        $(".choice").fadeOut(1000);
+    });
+
+    $("#easy").click(function(){
+        $("#easy").css("border-color", "green");
+        $("#normal").css("border-color", "black");
+        $("#unbeatable").css("border-color", "black");
+        difficulty = 0;
+    });
+
+    $("#normal").click(function(){
+        $("#easy").css("border-color", "black");
+        $("#normal").css("border-color", "orange");
+        $("#unbeatable").css("border-color", "black");
+        difficulty = 1;
+    });
+
+    $("#unbeatable").click(function(){
+        $("#easy").css("border-color", "black");
+        $("#normal").css("border-color", "black");
+        $("#unbeatable").css("border-color", "red");
+        difficulty = 2;
     });
 });
 
 //Javascript for tic-tac-toe
 var origBoard;
-const huPlayer = "O";
-const aiPlayer = "X";
 const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -37,8 +131,18 @@ function startGame() {
 
 function turnClick(square) {
     if (typeof origBoard[square.target.id] === 'number') {
-        turn(square.target.id, huPlayer);
-        if (!checkTie()) {
+        if (!checkTie() && noPlayers == 1) {
+            turn(square.target.id, huPlayer);
+        } else {
+            if (currentPlayer == 1 && !checkTie()) {
+                turn(square.target.id, huPlayer);
+                currentPlayer = 2;
+            } else if(currentPlayer == 2 && !checkTie()) {
+                turn(square.target.id, huPlayer2);
+                currentPlayer = 1;
+            }
+        }
+        if (!checkTie() && noPlayers === 1) {
             turn(bestSpot(), aiPlayer);
         }
     }
@@ -77,7 +181,25 @@ function gameOver(gameWon) {
     for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false);
     }
-    declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose!");
+    if (noPlayers === 1) {
+        declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose!");
+        if (gameWon.player === huPlayer && !checkTie()) {
+            p1score++;
+            document.getElementById("p1score").innerText = " " + p1score;
+        } else if (gameWon.player === aiPlayer && !checkTie()) {
+            aiscore++;
+            document.getElementById("p2score").innerText = " " + aiscore;
+        }
+    } else {
+        declareWinner(gameWon.player == huPlayer ? p1name + " wins!" : p2name + " wins!");
+        if (gameWon.player === huPlayer && !checkTie()) {
+            p1score++;
+            document.getElementById("p1score").innerText = " " + p1score;
+        } else if (gameWon.player === huPlayer2 && !checkTie()) {
+            p2score++;
+            document.getElementById("p2score").innerText = " " + p2score;
+        }
+    }
 }
 
 function declareWinner(result) {
@@ -90,10 +212,9 @@ function emptySquares() {
 }
 
 function bestSpot() {
-    if(difficulty === 0) {
+    if (difficulty === 0) {
         return emptySquares()[0];
-    }
-    else if (difficulty === 1) {
+    } else if (difficulty === 1) {
         var length = emptySquares().length;
         return emptySquares()[Math.floor(Math.random(length) + 1)];
     } else if (difficulty === 2) {
@@ -102,15 +223,19 @@ function bestSpot() {
 }
 
 function checkTie() {
-    if (emptySquares().length === 0) {
+
+    if (emptySquares().length === 0 && !checkWin(origBoard, "X") && !checkWin(origBoard, "O")) {
         for (var i = 0; i < cells.length; i++) {
-            cells[i].style.backgroundColor = "green";
             cells[i].removeEventListener('click', turnClick, false);
         }
-        declareWinner("Tie Game!");
-        return true;
+            declareWinner("Tie Game!");
+            return true;
     }
     return false;
+}
+
+function reset() {
+    location.reload();
 }
 
 // MINIMAX //
@@ -169,4 +294,3 @@ function minimax(newBoard, player) {
     }
     return moves[bestMove];
 }
-
